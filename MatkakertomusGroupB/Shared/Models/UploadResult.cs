1a6c0fb26c9366e3bad3db7af70686c83433f7b9
Jussi Kieleväinen <jussi.kielevainen@edu.savonia.fi>
2023-03-30 22:18:23 +0300

    file upload partially working

---
+++ MatkakertomusGroupB/Shared/Models/UploadResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatkakertomusGroupB.Shared.Models
+{
+    public class UploadResult
+    {
+        public string? FileName { get; set; }
+        public string? StoredFileName { get; set; }
+        // this will be used to randomize the uploaded filename on server
+    }
+}
