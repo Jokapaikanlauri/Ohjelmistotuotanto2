e9dc478a5e638820cb176ce8eff2265cae3c310a
Tuukka Kumpulainen <Tuukka.Kumpulainen@edu.savonia.fi>
2023-04-28 17:50:26 +0300

    Implemented Delete_OwnTrips()

--- MatkakertomusGroupB.Tests/SeleniumTests.cs
+++ MatkakertomusGroupB.Tests/SeleniumTests.cs
@@ -2288,7 +2288,7 @@ namespace MatkakertomusGroupB.Tests
 			keyElem = null;
 
 
-			//Expect to find the delete button
+			//Expect to find the delete button div
 			keyElemId = "editstory-div";
 			//Get element
 			keyElem = _webDriver.FindElement(By.Id(keyElemId));
@@ -2398,22 +2398,14 @@ namespace MatkakertomusGroupB.Tests
 		}
 
 		[Test, Order(24)]
-		public void Delete_OwnTrip()
+		public void Delete_OwnTrips()
 		{
-
-
-			//Boilerplate
-			Assert.Ignore();
-			/*
-			//Navigate to Own Trips, wait for add element to be enabled
+			//Navigate back to own trips
 			string linkText = "My Trips";
 			var elem = _webDriver.FindElement(By.PartialLinkText(linkText));
-			string actual = elem.GetAttribute("href").ToString();
-			string expected = "trips";
-			Assert.AreEqual(true, (actual.Contains(expected)), $"Expected nav menu my trips link to contain \"{expected}\", but it wasn't found. Actual: \"{actual}\"");
 			elem.Click();
 			//Expect to find page content
-			string keyElemId = "trip-razor-add";
+			string keyElemId = "owntrips-razor";
 			//Get element
 			var keyElem = _webDriver.FindElement(By.Id(keyElemId));
 			//Define wait time
@@ -2434,18 +2426,24 @@ namespace MatkakertomusGroupB.Tests
 			Assert.AreEqual(true, keyElem.Displayed, $"Expected to find page with element \"{keyElemId}\" via link with text \"{linkText}\" but it wasn't found.");
 
 
-			//Fill out the form
-			_webDriver.FindElement(By.Id("Input_Trip_StartDate")).SendKeys(tripPubStartDate);
-			_webDriver.FindElement(By.Id("Input_Trip_EndDate")).SendKeys(tripPubEndDate);
-			//_webDriver.FindElement(By.Id("Input_Trip_Private"));
 
-			//Proceed
-			_webDriver.FindElement(By.Id("addSubmit")).Click();
-
-
-			//The OK message should be displayed
-			keyElemId = "trip-added-alert";
+			//Find the previously added public trip element
+			keyElemId = $"privateitem-False" +
+			   $"-{DateTime.Parse(tripPubStartDate).ToString("yyyy-MM-dd")}" +
+			   $"-{DateTime.Parse(tripPubEndDate).ToString("yyyy-MM-dd")}";
+			//Get element
 			keyElem = _webDriver.FindElement(By.Id(keyElemId));
+			//Click the button inside the found element to manage trip
+			string expected = "tripManage";
+			keyElem.FindElement(By.Id(expected)).Click();
+
+
+
+			//Expect to find the div
+			keyElemId = "ownTripEdit-razor";
+			//Get element
+			keyElem = _webDriver.FindElement(By.Id(keyElemId));
+			//Define wait time
 			wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(waitB4FailSeconds));
 			//Wait for the Blazor to actually display the element (it's hidden initially due to loading...)
 			wait.Until(driver =>
@@ -2459,17 +2457,134 @@ namespace MatkakertomusGroupB.Tests
 					return false;
 				}
 			});
+			//If it was actually displayed this should resolve as "true, true"
+			Assert.AreEqual(true, keyElem.Displayed, $"Expected to find page with element \"{keyElemId}\" but it wasn't found.");
 
-			string keyElemHTML = keyElem.GetAttribute("innerHTML");
-			actual = "A new trip was created successfully!";
-			//If it was actually displayed and contained OK TEXT this should resolve as "true, true"
-			Assert.AreEqual(true, keyElemHTML.Contains(actual), $"Expected the page to display the OK message but it didn't. Messagebox HTML was:\n {keyElemHTML}");
+			//Try and remove the trip
+			//Expect to find the delete button
+			keyElemId = "deleteTripButton";
+			//Get element
+			keyElem = _webDriver.FindElement(By.Id(keyElemId));
+			keyElem.Click();
+
+
+
+
+			//Should reroute back to triplist, but we doing it manually
+
+
+			//Navigate back to own trips
+			linkText = "My Trips";
+			elem = _webDriver.FindElement(By.PartialLinkText(linkText));
+			elem.Click();
+			//Expect to find page content
+			keyElemId = "owntrips-razor";
+			//Get element
+			keyElem = _webDriver.FindElement(By.Id(keyElemId));
+			//Define wait time
+			wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(waitB4FailSeconds));
+			//Wait for the Blazor to actually display the element (it's hidden initially due to loading...)
+			wait.Until(driver =>
+			{
+				if (keyElem.Displayed)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			});
+			//If it was actually displayed this should resolve as "true, true"
+			Assert.AreEqual(true, keyElem.Displayed, $"Expected to find page with element \"{keyElemId}\" via link with text \"{linkText}\" but it wasn't found.");
+
+
+			//Find the previously added private(now public trip element
+			keyElemId = $"privateitem-False" +
+			   $"-{DateTime.Parse(tripPrivStartDate).ToString("yyyy-MM-dd")}" +
+			   $"-{DateTime.Parse(tripPrivEndDate).ToString("yyyy-MM-dd")}";
+			//Get element
+			keyElem = _webDriver.FindElement(By.Id(keyElemId));
+			//Click the button inside the found element to manage trip
+			expected = "tripManage";
+			keyElem.FindElement(By.Id(expected)).Click();
+
+
+
+			//Expect to find the div
+			keyElemId = "ownTripEdit-razor";
+			//Get element
+			keyElem = _webDriver.FindElement(By.Id(keyElemId));
+			//Define wait time
+			wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(waitB4FailSeconds));
+			//Wait for the Blazor to actually display the element (it's hidden initially due to loading...)
+			wait.Until(driver =>
+			{
+				if (keyElem.Displayed)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			});
+			//If it was actually displayed this should resolve as "true, true"
+			Assert.AreEqual(true, keyElem.Displayed, $"Expected to find page with element \"{keyElemId}\" but it wasn't found.");
+
+			//Try and remove the trip
+			//Expect to find the delete button
+			keyElemId = "deleteTripButton";
+			//Get element
+			keyElem = _webDriver.FindElement(By.Id(keyElemId));
+			keyElem.Click();
+
+
+			//Should reroute back to triplist, but we doing it manually
+
+
+			//Navigate back to own trips
+			linkText = "My Trips";
+			elem = _webDriver.FindElement(By.PartialLinkText(linkText));
+			elem.Click();
+			//Expect to find page content
+			keyElemId = "owntrips-razor";
+			//Get element
+			keyElem = _webDriver.FindElement(By.Id(keyElemId));
+			//Define wait time
+			wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(waitB4FailSeconds));
+			//Wait for the Blazor to actually display the element (it's hidden initially due to loading...)
+			wait.Until(driver =>
+			{
+				if (keyElem.Displayed)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			});
+			//If it was actually displayed this should resolve as "true, true"
+			Assert.AreEqual(true, keyElem.Displayed, $"Expected to find page with element \"{keyElemId}\" via link with text \"{linkText}\" but it wasn't found.");
+
+
+			//Find all if any
+			string keyElemClass = "list-item";
+			//Get element
+			var keyElems = _webDriver.FindElements(By.XPath($"//div[@class='{keyElemClass}']"));
+			//Click the button inside the found element to manage trip
+
+			int expectedInt = 0;
+			int actualInt = keyElems.Count;
+
+			Assert.AreEqual(0, keyElems.Count, $"Expected to find page without any  trips but they were found on the page.");
 
 			if (extraDelayEnabled)
 			{
 				Thread.Sleep(extraDelayInMilliSeconds);
 			}
-			*/
+
 		}
 
 		[Test, Order(25)]
